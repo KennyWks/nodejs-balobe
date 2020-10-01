b29cd6bde62cfe932597f838c027e8cc79b3f9e8
KennyWks <kenny.perulu@gmail.com>
2020-10-01 22:08:44 +0800

    add varible privateKey

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 var admin = require("firebase-admin");
-
+var privatekey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n");
+console.log(privatekey);
 var serviceAccount = {
   "type": process.env.FIREBASE_TYPE,
   "project_id": process.env.FIREBASE_PROJECT_ID,
   "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "private_key": privatekey,
   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   "client_id": process.env.FIREBASE_CLIENT_ID,
   "auth_uri": process.env.FIREBASE_AUTH_URI,
@@ -13,8 +14,6 @@ var serviceAccount = {
   "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_URL,
   "client_x509_cert_url": process.env.FIREBASE_CLIENT_URL
 };
-
-console.log(process.env.FIREBASE_PROJECT_ID)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
