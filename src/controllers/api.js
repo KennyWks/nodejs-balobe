d7f1c7f82f7fa152d01ffd566930ce39e88331c1
KennyWks <kenny.perulu@gmail.com>
2020-11-12 13:08:32 +0800

    add rajaongkir services new style

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -1,28 +1,23 @@
-var request = require("request");
+var rp = require('request-promise');
 
 exports.GetAllProvincesData = (req, res) => {
 
-    if(req.params.id) {
-        var options = {
-            method: 'GET',
-            url: 'https://api.rajaongkir.com/starter/province',
-            qs: {id: `${req.params.id}`},
-            headers: {key: '96ac5b6cdb274fd84c01ec9ed3ff1bca'}
-         };    
-    } else {
-        var options = {
-            method: 'GET',
-            url: 'https://api.rajaongkir.com/starter/province',
-            headers: {key: '96ac5b6cdb274fd84c01ec9ed3ff1bca'}
-         };
-    }
-    
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-
-        console.log(body);
-    });
-    
+    var options = {
+        uri: 'https://api.rajaongkir.com/starter/province',
+        qs: {
+            qs: {id: `${req.params.id ? req.params.id : ''}`},
+        },
+        headers: {key: '96ac5b6cdb274fd84c01ec9ed3ff1bca'},
+        json: true
+    };
+     
+    rp(options)
+        .then(function (repos) {
+            console.log('User has %d repos', repos.length);
+        })
+        .catch(function (err) {
+            // API call failed...
+        });
 };
 
 // exports.GetDetailCategoryController = async (req, res) => {
