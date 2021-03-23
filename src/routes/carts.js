25f404f75b22accdf59780644ce24ce9c63c918b
KennyWks <kenny.perulu@gmail.com>
2021-03-23 21:02:28 +0800

    add routes api for update carts by id carts

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -1,18 +1,18 @@
 const CartsRouter = require("express").Router();
 const AuthUser = require("../middleware/auth_user");
 const {
-    CreateCartsController,
-    GetAllCartsController,
-    GetDetailCartsController,
-    UpdateCartsController,
-    CheckOutContoller
+  CreateCartsController,
+  GetAllCartsController,
+  GetDetailCartsController,
+  UpdateCartsController,
+  CheckOutContoller,
 } = require("../controllers/carts");
 
 // CartsRouter.post("/", CreateCategory);
 CartsRouter.post("/", AuthUser, CreateCartsController);
 CartsRouter.get("/", AuthUser, GetAllCartsController);
 CartsRouter.get("/:id", AuthUser, GetDetailCartsController);
-CartsRouter.post("/update/:id", AuthUser, UpdateCartsController);
+CartsRouter.patch("/update/:id", AuthUser, UpdateCartsController);
 CartsRouter.post("/:id", AuthUser, CheckOutContoller);
 
 module.exports = CartsRouter;
