25f2b6c02e577288d2410fb2ad7c694057a54f34
KennyWks <kenny.perulu@gmail.com>
2021-03-29 23:25:05 +0800

    fixing search again

--- src/models/item.js
+++ src/models/item.js
@@ -32,7 +32,7 @@ exports.GetAllItemModel = (params) => {
 
   return new Promise((resolve, reject) => {
     const { limit, page, sort, search } = params;
-    const condition = ` ${search ? `WHERE items.name LIKE '%${search}%' GROUP BY items.id_item` : "GROUP BY items.id_item"}
+    const condition = ` ${search ? `WHERE items.name LIKE '%${search}%'` : "GROUP BY items.id_item"}
                         ${sort ? `ORDER BY ${sort.key} ${sort.value}` : ""} LIMIT ${parseInt(limit)} OFFSET ${parseInt(page) - 1}`;
 
     runQuery(`SELECT COUNT(*) AS total FROM items ${condition.substring(0, condition.indexOf("LIMIT"))};
