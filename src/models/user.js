90d214b3088c5a9adf67c60b20b2afb0461fdb5d
KennyWks <kenny.perulu@gmail.com>
2021-05-20 14:18:38 +0800

    edit model get all user by admin

--- src/models/user.js
+++ src/models/user.js
@@ -45,14 +45,14 @@ exports.GetAllUserModel = (params) => {
         } = params;
 
         const condition = `
-        ${search ? `WHERE username LIKE '%${search}%'` : ""}
+        ${search ? `WHERE fullname LIKE '%${search}%'` : ""}
         ${sort ? `ORDER BY ${sort.key} ${sort.value}` : "" } LIMIT ${parseInt(limit)} OFFSET ${parseInt(page) - 1}`;
         
         const join = `
                     JOIN user_profiles
                     ON user_profiles.id_user = users.id_user
                     `;
-                    
+
         const select = "`users`.`id_user`, `user_profiles`.`fullname`, `user_profiles`.`gender`, `user_profiles`.`picture`, `user_profiles`.`address`, `user_profiles`.`email`, `user_profiles`.`phone`, `user_profiles`.`balance`, `users`.`status`";
 
         runQuery(`
