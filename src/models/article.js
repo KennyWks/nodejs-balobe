0ad95826dd750908b3894ca390c4b62ad2406343
KennyWks <kenny.perulu@gmail.com>
2024-02-07 14:48:04 +0800

    change query in get dat articles model

--- src/models/article.js
+++ src/models/article.js
@@ -1,81 +1,114 @@
-const {
-  runQuery
-} = require("../config/db");
+const { runQuery } = require("../config/db");
 
 exports.CreateArticleModel = (data) => {
   return new Promise((resolve, reject) => {
-    runQuery(`
-INSERT INTO articles(id_category, name, description, image) values('${data.id_category}','${data.name}','${data.description}','${data.image}')`, (err, result) => {
-      if (err) {
-        return reject(new Error(err));
+    runQuery(
+      `
+INSERT INTO articles(id_category, name, description, image) values('${data.id_category}','${data.name}','${data.description}','${data.image}')`,
+      (err, result) => {
+        if (err) {
+          return reject(new Error(err));
+        }
+        return resolve(result);
       }
-      return resolve(result);
-    });
+    );
   });
 };
 
 exports.GetAllArticleModel = (params) => {
   return new Promise((resolve, reject) => {
-    const {
-      limit,
-      page,
-      sort,
-      search
-    } = params;
+    const { limit, page, sort, search } = params;
+
+    const join = `JOIN category ON category.id_category = articles.id_category`;
+    const column = `articles.id_article,
+                    articles.name AS article_name,
+                    articles.description, 
+                    articles.image, 
+                    category.hs_code, 
+                    category.name AS category_name`;
+
     const condition = `
       ${search ? `WHERE name LIKE '%${search}%'` : ""}
-      ${sort ? `ORDER BY ${sort.key} ${sort.value}` : "" } LIMIT ${parseInt(limit)} OFFSET ${parseInt(page) - 1}`;
-    runQuery(`
-      SELECT COUNT(*) AS total FROM articles ${condition.substring(0,  condition.indexOf("LIMIT"))};
-      SELECT * FROM articles ${condition}
-      `, (err, result) => {
-      if (err) {
-        return reject(new Error(err));
-      }
-      return resolve(result);
-    });
-  });
-}
+      ${sort ? `ORDER BY ${sort.key} ${sort.value}` : ""} LIMIT ${parseInt(
+      limit
+    )} OFFSET ${parseInt(page) - 1}`;
 
-exports.GetDetailArticleModel = (id_article) => {
-  return new Promise((resolve, reject) => {
-    runQuery(`SELECT * FROM articles WHERE id_article=${id_article}`, (err, result) => {
-      if (err) {
-        return reject(new Error(err));
-      }
-      return resolve(result);
-    });
-  });
-}
-
-exports.UpdateArticleModel = (id_article, body, path) => {
-  return new Promise((resolve, reject) => {
-      runQuery(`UPDATE articles SET ${Object.keys(body).map((v) => `${v} = '${body[v]}'`).join(",")} WHERE id_article=${id_article}`, (err, result) => {
-        if (err) {
-          return reject(new Error(err));
-        }
-        runQuery(`UPDATE articles SET image = '${path}' WHERE id_article=${id_article}`, (err, result) => {
-          if (err) {
-            return reject(new Error(err));
-          }
-          return resolve(result);
-        });
-      });
-    });
-}
-
-exports.DeleteArticleModel = (id_article) => {
-  return new Promise((resolve, reject) => {
-    runQuery(`SELECT * FROM articles WHERE id_article=${id_article}`, (err, result) => {
-      if (err || !result[1][0]) {
-        return reject(new Error(`articles with id : ${id_article} not exists`));
-      }
-      runQuery(`DELETE FROM articles WHERE id_article=${id_article}`, (err, result) => {
+    runQuery(
+      `
+      SELECT COUNT(*) AS total FROM articles ${condition.substring(
+        0,
+        condition.indexOf("LIMIT")
+      )};
+      SELECT ${column} FROM articles ${join} ${condition}
+      `,
+      (err, result) => {
         if (err) {
           return reject(new Error(err));
         }
         return resolve(result);
-      });
-    });
+      }
+    );
   });
-}
+};
+
+exports.GetDetailArticleModel = (id_article) => {
+  return new Promise((resolve, reject) => {
+    runQuery(
+      `SELECT * FROM articles WHERE id_article=${id_article}`,
+      (err, result) => {
+        if (err) {
+          return reject(new Error(err));
+        }
+        return resolve(result);
+      }
+    );
+  });
+};
+
+exports.UpdateArticleModel = (id_article, body, path) => {
+  return new Promise((resolve, reject) => {
+    runQuery(
+      `UPDATE articles SET ${Object.keys(body)
+        .map((v) => `${v} = '${body[v]}'`)
+        .join(",")} WHERE id_article=${id_article}`,
+      (err, result) => {
+        if (err) {
+          return reject(new Error(err));
+        }
+        runQuery(
+          `UPDATE articles SET image = '${path}' WHERE id_article=${id_article}`,
+          (err, result) => {
+            if (err) {
+              return reject(new Error(err));
+            }
+            return resolve(result);
+          }
+        );
+      }
+    );
+  });
+};
+
+exports.DeleteArticleModel = (id_article) => {
+  return new Promise((resolve, reject) => {
+    runQuery(
+      `SELECT * FROM articles WHERE id_article=${id_article}`,
+      (err, result) => {
+        if (err || !result[1][0]) {
+          return reject(
+            new Error(`articles with id : ${id_article} not exists`)
+          );
+        }
+        runQuery(
+          `DELETE FROM articles WHERE id_article=${id_article}`,
+          (err, result) => {
+            if (err) {
+              return reject(new Error(err));
+            }
+            return resolve(result);
+          }
+        );
+      }
+    );
+  });
+};
